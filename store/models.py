16804eb012719824fb0f92e32afe9d5c9a318ee7
Abhishant Singh <abhishantsingh05@gmail.com>
2022-05-31 22:36:38 +0530

    made migrations

--- store/models.py
+++ store/models.py
@@ -1,3 +1,32 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    sku = models.CharField(max_length=255,primary_key=True)
+    title = models.CharField(max_length=255) # max lenght is mandatory
+    slug = models.SlugField(default='-')
+    description = models.TextField
+    mobile = models.BigIntegerField
+    price = models.DecimalField(max_digits=6, decimal_places=2) # the two arguments are always required in decimal field
+    last_update = models.DateField(auto_now=True)
+    collection = models.ForeignKey("Collection",on_delete=models.PROTECT)
+
+class Customer(models.Model):
+    MEMBERSHIP_VALUES = [
+        ('B',"BRONZE"),
+        ('G',"GOLD")
+    ]
+    email = models.EmailField(unique=True) #only unique
+    birth_date = models.DateField(null=True) # nullable
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_VALUES, default='B')
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer,on_delete=models.CASCADE, primary_key=True)# mandatory arguments
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
